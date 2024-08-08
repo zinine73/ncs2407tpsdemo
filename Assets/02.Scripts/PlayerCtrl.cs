6942820df0f320a1854b56622db7102e8a179e20
zinine <zinine@naver.com>
2024-08-08 12:45:55 +0900

    vector3

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private Transform tr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log("h=" + h);
+        Debug.Log("v=" + v);
+
+        //transform.position += new Vector3(0, 0, 1);
+        //transform.position += Vector3.forward * 1;
+
+        tr.position += Vector3.forward * 1;
     }
 }
