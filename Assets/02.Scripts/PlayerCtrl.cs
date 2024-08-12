49fba1008195604f18619e4ade0058df36a835bc
zinine <zinine@naver.com>
2024-08-12 15:44:47 +0900

    animation

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    [SerializeField] private Transform tr;
+    private Transform tr;
+    private Animation anim;
     public float moveSpeed = 8.0f;
+    public float turnSpeed = 80.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+
+        anim.Play("Idle");
     }
 
     // Update is called once per frame
@@ -18,11 +23,36 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
-        Debug.Log("h=" + h);
-        Debug.Log("v=" + v);
+        float r = Input.GetAxis("Mouse X");
 
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
+
+        PlayerAnim(h, v);
+    }
+
+    void PlayerAnim(float h, float v)
+    {
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.25f);
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.25f);
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade("RunR", 0.25f);
+        }
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade("RunL", 0.25f);
+        }
+        else
+        {
+            anim.CrossFade("Idle", 0.25f);
+        }
     }
 }
