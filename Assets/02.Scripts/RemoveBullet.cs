9cad18f01bf6b326f029fa5b82064a8b51d1e206
zinine <zinine@naver.com>
2024-08-23 15:49:42 +0900

    particle

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("BULLET"))
         {
+            ContactPoint cp = collision.GetContact(0);
+            // 충돌한 총알의 법선벡터를 쿼터니언으로 변환
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+
+            Destroy(spark, 0.5f);
+
             Destroy(collision.gameObject);
         }
     }
