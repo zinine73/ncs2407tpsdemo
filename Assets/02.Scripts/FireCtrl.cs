79dd2e2bff607a8271771073a3f78d2a596d3458
zinine <zinine@naver.com>
2024-08-28 15:51:36 +0900

    sound

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
     public Transform firePos;
+    public AudioClip fireSfx;
+
+    private new AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,5 +30,7 @@ public class FireCtrl : MonoBehaviour
     {
         // Bullet prefab을 동적으로 생성 (생성할 객체, 위치, 회전)
         Instantiate(bullet, firePos.position, firePos.rotation);
+
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
