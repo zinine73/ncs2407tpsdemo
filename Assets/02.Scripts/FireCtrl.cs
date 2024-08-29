583ea7ba5b8afcadadc5f8249026d65cc4e106a9
zinine73 <zinine@naver.com>
2024-08-29 11:32:10 +0900

    coroutine

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -10,11 +10,17 @@ public class FireCtrl : MonoBehaviour
     public AudioClip fireSfx;
 
     private new AudioSource audio;
+    private MeshRenderer muzzleFlash;
 
     // Start is called before the first frame update
     void Start()
     {
         audio = GetComponent<AudioSource>();
+
+        // FirePos 하위에 있는 MuzzleFlash의 Material 컴포넌트 추출
+        muzzleFlash = firePos.GetComponentInChildren<MeshRenderer>();
+
+        muzzleFlash.enabled = false;
     }
 
     // Update is called once per frame
@@ -32,5 +38,18 @@ public class FireCtrl : MonoBehaviour
         Instantiate(bullet, firePos.position, firePos.rotation);
 
         audio.PlayOneShot(fireSfx, 1.0f);
+
+        // 총구 화염 효과 코루틴 함수 호출
+        StartCoroutine(ShowMuzzleFlash());
+    }
+
+    IEnumerator ShowMuzzleFlash()
+    {
+        muzzleFlash.enabled = true;
+
+        // 0.2초 동안 대기하는 동안 메시지 루프로 제어권 양보
+        yield return new WaitForSeconds(0.2f);
+
+        muzzleFlash.enabled = false;
     }
 }
