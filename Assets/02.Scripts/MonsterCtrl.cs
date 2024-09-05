1290af7d23e54fae033316fd1e057a2df81e348e
zinine73 <zinine@naver.com>
2024-09-05 12:21:53 +0900

    FSM

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -5,22 +5,97 @@ using UnityEngine.AI;
 
 public class MonsterCtrl : MonoBehaviour
 {
+    public enum State
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    public State state = State.IDLE;
+    public float traceDist = 10.0f;
+    public float attackDist = 2.0f;
+    public bool isDie = false;
+
     private Transform monsterTr;
     private Transform playerTr;
     private NavMeshAgent agent;
+    private Animator anim;
 
-    // Start is called before the first frame update
     void Start()
     {
         monsterTr = GetComponent<Transform>();
         playerTr = GameObject.FindWithTag("PLAYER").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(playerTr.position);
+        anim = GetComponent<Animator>();
+
+        // 몬스터의 상태를 체크하는 코루틴 호출
+        StartCoroutine(CheckMonsterState());
+        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 호출
+        StartCoroutine(MonsterAction());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MonsterAction()
     {
-        
+        while (!isDie)
+        {
+            switch (state)
+            {
+                case State.IDLE:
+                    agent.isStopped = true;
+                    // Animator의 IsTrace변수를 false로 설정
+                    anim.SetBool("IsTrace", false);
+                    break;
+                case State.ATTACK:
+                    break;
+                case State.TRACE:
+                    agent.SetDestination(playerTr.position);
+                    agent.isStopped = false;
+                    anim.SetBool("IsTrace", true);
+                    break;
+                case State.DIE:
+                    break;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    IEnumerator CheckMonsterState()
+    {
+        while(!isDie)
+        {
+            // 0.3초 중지 하는 동안 양보
+            yield return new WaitForSeconds(0.3f);
+
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
+
+            if (distance <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (state == State.TRACE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, traceDist);
+        }
+        if (state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackDist);
+        }
     }
 }
