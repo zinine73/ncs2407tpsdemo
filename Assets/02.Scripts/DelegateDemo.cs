452d896f51482451dcda80e6c63f0313bc95ccc6
zinine73 <zinine@naver.com>
2024-09-13 15:48:20 +0900

    button

--- Assets/02.Scripts/DelegateDemo.cs
+++ Assets/02.Scripts/DelegateDemo.cs
@@ -27,5 +27,17 @@ public class DelegateDemo : MonoBehaviour
         Debug.Log($"Minus = {minus}");
         sumHandler += Sum;
         sumHandler += Minus;
+
+        // 델리게이트 변수에 람다식 선언
+        sumHandler = (float a, float b) => (a + b);
+        float sum2 = sumHandler(10.0f, 5.0f);
+        Debug.Log($"Sum2 = {sum2}");
+
+        // 델리게이트 변수에 무명 메서드 연결
+        sumHandler = delegate (float a, float b) { return a + b; };
+        float sum3 = sumHandler(2.0f, 3.0f);
+        Debug.Log($"Sum3 = {sum3}");
+
+        sumHandler = (float x, float y) => (x - y);
     }
 }
--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+
+    private UnityAction action;
+
     void Start()
     {
-        
-    }
+        // UnityAction을 사용한 이벤트 연결 방식
+        action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(action);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 무명메서드를 사용한 이벤트 연결 방식
+        optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
+
+        // 람다식을 사용한 이벤트 연결 방식
+        shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
     }
 
     public void OnButtonClick(string msg)
